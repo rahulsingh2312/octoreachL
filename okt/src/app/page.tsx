df0f2ef1cfde4de8180956a5baef88f8492f9621
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-11-08 18:24:52 +0530

    done

---
+++ okt/src/app/page.tsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+   <div className="-mt-24 ">
+      <iframe
+  width="100%"
+  height="100%"
+  // style={{ backgroundColor: "#5D5BB9" }}
+  className="h-[5000px]   md:h-[3800px] "
+  src="http://localhost:3000/services"
+  title="description"
+  scrolling="no"
+/>
+      
+   </div>
+  );
+}
