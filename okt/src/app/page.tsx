7e855f961609d21835a0d29821ac020e01b08359
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-11-08 18:53:46 +0530

    done

--- okt/src/app/page.tsx
+++ okt/src/app/page.tsx
@@ -1,17 +1,14 @@
-
 export default function Home() {
   return (
-   <div className="-mt-24 ">
+    <div className="-mt-24">
       <iframe
-  width="100%"
-  height="100%"
-  // style={{ backgroundColor: "#5D5BB9" }}
-  className="h-[5000px]   md:h-[3800px] "
-  src="https://octoreachdigital.com/services"
-  title="description"
-  scrolling="no"
-/>
-      
-   </div>
+        width="100%"
+        height="100%"
+        className="w-full iframe-height-335-340 iframe-height-340-350 iframe-height-351-363 iframe-height-366-372 md:h-[3800px]"
+        src="https://octoreachdigital.com/services"
+        title="description"
+        scrolling="no"
+      />
+    </div>
   );
 }
