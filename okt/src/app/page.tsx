10449a2016e5bf9c93a4918e136f4e05864a321d
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-11-10 01:00:27 +0530

    css

--- okt/src/app/page.tsx
+++ okt/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <iframe
         width="100%"
         height="100%"
-        className="w-full iframe-height-335-340 iframe-height-340-350 iframe-height-351-363 iframe-height-366-372 md:h-[3800px]"
+        className="w-full iframe-height-335-340 iframe-height-340-350 iframe-height-351-363 iframe-height-366-372 md:h-[2400px]"
         src="https://octoreachdigital.com/services"
         title="description"
         scrolling="no"
